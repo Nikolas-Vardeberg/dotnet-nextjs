7a7fdc8dc69f20a69939d66f845f2c9b6a44fada
niklusrook <nikolas.vardeberg1@gmail.com>
2025-01-05 21:55:05 +0100

    adding database

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,13 @@
 using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
 
 var bld = WebApplication.CreateBuilder();
 bld.Services.AddFastEndpoints();
+
+bld.Services.AddDbContext<DataContext>(options => {
+    options.UseNpgsql(bld.Configuration.GetConnectionString("WebApiDatabase"));
+});
 
 var app = bld.Build();
 app.UseFastEndpoints();
